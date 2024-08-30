bbe76a519f609604f15be93bbd0a21b0253cb093
JoFrhwld <JoFrhwld@gmail.com>
2024-08-30 18:41:02 -0400

    docs and remove function

--- R/codeannotate.R
+++ R/codeannotate.R
@@ -1,4 +1,17 @@
 #' Code Annotate
+#'
+#' @description
+#' Add code annotation markup to a code chunk in a quarto document.
+#'
+#' @details
+#' After installing this RStudio addin, when lines of code are selected,
+#' the markup for [quarto code annotations](https://quarto.org/docs/authoring/code-annotation.html)
+#' will be added.
+#'
+#' Setting up a custom keybinding is recommended.
+#'
+#' @seealso [remove_codeannotate()]
+#'
 #' @export
 codeannotate <- function(){
 
@@ -7,11 +20,7 @@ codeannotate <- function(){
   beginning <- selection$range$start
   ending <- selection$range$end
 
-  longest <- selection$text |>
-    stringr::str_split("\\n", simplify = T) |>
-    stringr::str_remove("#\\s*<\\d+>$") |>
-    stringr::str_trim("right") ->
-    code_lines
+  code_lines <- get_code_lines(selection)
 
   code_lengths <- nchar(code_lines)
   longest <- max(code_lengths)
@@ -52,6 +61,44 @@ codeannotate <- function(){
 }
 
 
+#' Remove Code Annotation
+#'
+#' @description
+#' Remove code annotation markup.
+#'
+#' @details
+#' If a selected line of code has quarto code annotation markup, this
+#' will remove it.
+#'
+#' Setting up a custom keybinding is recommended.
+#'
+#' @seealso [codeannotate()]
+#'
+#' @export
+remove_codeannotate <- function(){
+  selection <- get_selection()
+  beginning <- selection$range$start
+  ending <- selection$range$end
+
+  code_lines <- get_code_lines(selection)
+
+  ranges <- lapply(
+    seq(
+      beginning["row"],
+      ending["row"]
+    ),
+    \(x) rstudioapi::document_range(
+      start = c(x,1),
+      end = c(x, Inf)
+    )
+  )
+
+  rstudioapi::modifyRange(
+    location = ranges,
+    text = code_lines
+  )
+}
+
 
 get_selection <- function(){
   context <- rstudioapi::getSourceEditorContext()
@@ -70,6 +117,18 @@ get_selection <- function(){
     rstudioapi::primary_selection()
 
   return(selection)
+}
+
+get_code_lines <- function(selection){
+
+  selection$text |>
+    stringr::str_split("\\n", simplify = T) |>
+    stringr::str_remove("#\\s*<\\d+>$") |>
+    stringr::str_trim("right") ->
+    code_lines
+
+  return(code_lines)
+
 }
 
 expand_selection <- function(selection){
